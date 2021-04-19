5b9c51e29b17cd366837bac96191329dabc9c6cf
weli <l.weinan@gmail.com>
2021-04-19 23:35:52 +0800

    httpRequest context

--- src/main/java/org/acme/getting/started/GreetingResource.java
+++ src/main/java/org/acme/getting/started/GreetingResource.java
@@ -1,16 +1,27 @@
 package org.acme.getting.started;
 
+
+import org.jboss.resteasy.spi.HttpRequest;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+
 
 @Path("/hello")
 public class GreetingResource {
 
+    // https://github.com/quarkusio/quarkus/issues/4125
+    // https://github.com/quarkusio/quarkus/pull/5581
+    @Context
+    private HttpRequest request;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasy";
+        return "<><><>" + request;
     }
 }
